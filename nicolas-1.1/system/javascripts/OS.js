69d2904c0d113452bc5f483b2ab158fc56651d2e
NicolasSchwarzer <NicolasSchwarzer@users.noreply.github.com>
2016-02-10 22:30:08 +0800

    System layer: OS.js done!

---
+++ nicolas-1.1/system/javascripts/OS.js
@@ -0,0 +1,41 @@
+var OS = {};
+
+~function() {
+
+	var userAgent = navigator.userAgent;
+
+	Object.defineProperties(this, {
+
+		'isIOS': {
+
+			get: function() {
+
+				return /iPhone|iPad/.test(userAgent);	
+			}
+		},
+
+		'isAndroid': {
+
+			get: function() {
+
+				return /Android/.test(userAgent);
+			}
+		},
+
+		'isMac': {
+
+			get: function() {
+
+				return /Mac/.test(userAgent) && !this.isIOS;
+			}
+		},
+
+		'isWindows': {
+
+			get: function() {
+
+				return /Windows/.test(userAgent);
+			}
+		}
+	});
+}.call(OS);
