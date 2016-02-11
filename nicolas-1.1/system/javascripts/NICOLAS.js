4f548a9509c48cf1f192f5bb3cd4e9b1512c2de1
NicolasSchwarzer <NicolasSchwarzer@users.noreply.github.com>
2016-02-11 23:06:08 +0800

    System layer: NICOLAS.js initialization!

---
+++ nicolas-1.1/system/javascripts/NICOLAS.js
@@ -0,0 +1,22 @@
+var NICOLAS = {};
+
+~function() {
+
+	var prototype = this;
+
+	prototype.initialize = function(el) {
+
+
+	};
+
+	prototype.ready = function(el) {
+
+
+	};
+
+	prototype.destroy = function(el) {
+
+
+	};
+
+}.call(NICOLAS);
